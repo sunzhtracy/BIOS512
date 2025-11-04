5eeeff474038dce4fa84e024f5b61245e67e25d6
sunzhtracy <156151552+sunzhtracy@users.noreply.github.com>
2025-11-04 15:29:26 -0500

    Update install.R

--- install.R
+++ install.R
@@ -1,5 +1,6 @@
 install.packages("tidyverse", dependencies = TRUE)
 install.packages("rmarkdown", dependencies = TRUE)
 install.packages("IRkernel", dependencies = TRUE)
+install.packages("glmnet", dependencies = TRUE)
 
 IRkernel::installspec(user = FALSE)
